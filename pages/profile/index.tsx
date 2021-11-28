c49ddd83d0f57ab0e7684360787981f06b5b1ef8
mystique09 <knightkrtpto7@gmail.com>
2021-11-28 22:44:40 +0800

    add profile index

--- pages/profile/index.tsx
+++ pages/profile/index.tsx
@@ -1,16 +1,28 @@
 import redirectTo from "@/helpers/redirect_to";
-import { GetServerSideProps } from "next"
+import {
+  GetServerSideProps
+} from "next"
+import User from "@/models/user";
 
-const Profile = () => {
+const Profile = ({ userInfo }) => {
   return <h1>Profile</h1>
 }
 
 export const getServerSideProps = async (context) => {
-
-  if(!context.req.cookies?.session_id) {
+  if (!context.req.cookies?.session_id) {
     return redirectTo("/auth");
   }
-
+/*
+  const userInfo = await User.findById({
+    _id: user._id
+  }).select('username email');
+  if (userInfo) {
+    return {
+      props: {
+        user: JSON.stringify(userInfo)
+      }
+    }
+  }*/
   return {
     props: {}
   }
